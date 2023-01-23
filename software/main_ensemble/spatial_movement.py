38914b7f9a01bc69f86231cfb75362314e6ef26e
Dhruv Sheth <dhruvsheth.linkit@gmail.com>
2023-01-24 00:49:29 +0530

    Create spatial_movement.py

---
+++ software/main_ensemble/spatial_movement.py
@@ -0,0 +1,251 @@
+#!/usr/bin/env python3
+
+from pathlib import Path
+import sys
+import cv2
+import depthai as dai
+import numpy as np
+import time
+
+import time
+from signal import pause
+from buildhat import Motor
+
+from datetime import datetime
+from time import sleep
+
+motor_gripper = Motor('A')
+motor_gripper_control = Motor('B')
+motor_body_control = Motor('C')
+motor_body_movement = Motor('D')
+motor_body_control.run_to_position(10, speed=60, direction='shortest')
+motor_gripper_control.run_to_position(125, speed=60, direction='anticlockwise')
+
+
+#motor_gripper.set_default_speed(40)
+#motor_gripper_control.set_default_speed(50)
+#motor_body_control.set_default_speed(50)
+#motor_body_movement.set_default_speed(60)
+
+print("Position Motor Gripper Port A", motor_gripper.get_aposition())
+print("Position Motor Gripper Control Port B", motor_gripper.get_aposition())
+print("Position Motor Body Control Port C", motor_gripper.get_aposition())
+print("Position Motor Body Movement Port D", motor_gripper.get_aposition())
+
+def closeGripper():
+    motor_gripper.run_to_position(140, speed=30, direction='clockwise')
+    motor_body_movement.run_to_position(180, speed=30, direction='clockwise')
+    motor_body_movement.run_to_position(-180, speed=30, direction='anticlockwise')
+    
+def openGripper():
+    motor_gripper.run_to_position(13, speed=30, direction='anticlockwise')
+
+
+def bodyMovement(x, y, z):
+    if x<((150)-85):
+        #move left
+        #motor_body_movement.run_for_degrees(20, speed=50)
+        motor_body_movement.start(30)
+        print("left")
+    elif x>((150)+85):
+        #move right
+        #motor_body_movement.run_for_degrees(20, speed=-50)
+        motor_body_movement.start(-30)
+        print("right")
+    elif y<((150)-85):
+        #move left
+        motor_body_control.start(-50)
+        print("up")
+        print("Position Motor Body Control Port C", motor_gripper.get_aposition())
+    elif y>((150)+85):
+        #move right
+        motor_body_control.start(30)
+        print("down")
+        print("Position Motor Body Control Port C", motor_gripper.get_aposition())
+
+    elif z<(500):
+        #move left
+        motor_gripper_control.start(50)
+        print("ahead")
+        print("Position Motor Gripper Control Port B", motor_gripper.get_aposition())
+
+    elif z>(800):
+        #move right
+        motor_gripper_control.start(-50)
+        print("behind")
+        print("Position Motor Gripper Control Port B", motor_gripper.get_aposition())
+
+    else:
+        #stop motors
+        motor_body_movement.stop()
+        motor_body_control.stop()
+        motor_gripper_control.stop()
+        motor_gripper.stop()
+        print("no movement")
+
+   
+lego = True    
+
+'''
+Spatial detection network demo.
+    Performs inference on RGB camera and retrieves spatial location coordinates: x,y,z relative to the center of depth map.
+'''
+
+# Get argument first
+nnBlobPath = str((Path(__file__).parent / Path('/home/raspberry/depthai-python/examples/models/face-detection-retail-0004.blob')).resolve().absolute())
+if len(sys.argv) > 1:
+    nnBlobPath = sys.argv[1]
+
+if not Path(nnBlobPath).exists():
+    import sys
+    raise FileNotFoundError(f'Required file/s not found, please run "{sys.executable} install_requirements.py"')
+
+# MobilenetSSD label texts
+labelMap = ["", "face"]
+
+syncNN = True
+
+# Create pipeline
+pipeline = dai.Pipeline()
+
+# Define sources and outputs
+camRgb = pipeline.create(dai.node.ColorCamera)
+spatialDetectionNetwork = pipeline.create(dai.node.MobileNetSpatialDetectionNetwork)
+monoLeft = pipeline.create(dai.node.MonoCamera)
+monoRight = pipeline.create(dai.node.MonoCamera)
+stereo = pipeline.create(dai.node.StereoDepth)
+
+xoutRgb = pipeline.create(dai.node.XLinkOut)
+xoutNN = pipeline.create(dai.node.XLinkOut)
+xoutDepth = pipeline.create(dai.node.XLinkOut)
+
+xoutRgb.setStreamName("rgb")
+xoutNN.setStreamName("detections")
+xoutDepth.setStreamName("depth")
+
+# Properties
+camRgb.setPreviewSize(300, 300)
+camRgb.setResolution(dai.ColorCameraProperties.SensorResolution.THE_1080_P)
+camRgb.setInterleaved(False)
+camRgb.setColorOrder(dai.ColorCameraProperties.ColorOrder.BGR)
+
+monoLeft.setResolution(dai.MonoCameraProperties.SensorResolution.THE_400_P)
+monoLeft.setBoardSocket(dai.CameraBoardSocket.LEFT)
+monoRight.setResolution(dai.MonoCameraProperties.SensorResolution.THE_400_P)
+monoRight.setBoardSocket(dai.CameraBoardSocket.RIGHT)
+
+# Setting node configs
+stereo.setDefaultProfilePreset(dai.node.StereoDepth.PresetMode.HIGH_DENSITY)
+# Align depth map to the perspective of RGB camera, on which inference is done
+stereo.setDepthAlign(dai.CameraBoardSocket.RGB)
+stereo.setOutputSize(monoLeft.getResolutionWidth(), monoLeft.getResolutionHeight())
+
+spatialDetectionNetwork.setBlobPath(nnBlobPath)
+spatialDetectionNetwork.setConfidenceThreshold(0.5)
+spatialDetectionNetwork.input.setBlocking(False)
+spatialDetectionNetwork.setBoundingBoxScaleFactor(0.5)
+spatialDetectionNetwork.setDepthLowerThreshold(100)
+spatialDetectionNetwork.setDepthUpperThreshold(5000)
+
+
+# Linking
+monoLeft.out.link(stereo.left)
+monoRight.out.link(stereo.right)
+
+camRgb.preview.link(spatialDetectionNetwork.input)
+if syncNN:
+    spatialDetectionNetwork.passthrough.link(xoutRgb.input)
+else:
+    camRgb.preview.link(xoutRgb.input)
+
+spatialDetectionNetwork.out.link(xoutNN.input)
+
+stereo.depth.link(spatialDetectionNetwork.inputDepth)
+spatialDetectionNetwork.passthroughDepth.link(xoutDepth.input)
+
+# Connect to device and start pipeline
+with dai.Device(pipeline) as device:
+
+    # Output queues will be used to get the rgb frames and nn data from the outputs defined above
+    previewQueue = device.getOutputQueue(name="rgb", maxSize=4, blocking=False)
+    detectionNNQueue = device.getOutputQueue(name="detections", maxSize=4, blocking=False)
+    depthQueue = device.getOutputQueue(name="depth", maxSize=4, blocking=False)
+
+    startTime = time.monotonic()
+    counter = 0
+    fps = 0
+    color = (255, 255, 255)
+
+    while True:
+        inPreview = previewQueue.get()
+        inDet = detectionNNQueue.get()
+        depth = depthQueue.get()
+
+        counter+=1
+        current_time = time.monotonic()
+        if (current_time - startTime) > 1 :
+            fps = counter / (current_time - startTime)
+            counter = 0
+            startTime = current_time
+
+        frame = inPreview.getCvFrame()
+
+        depthFrame = depth.getFrame() # depthFrame values are in millimeters
+
+        depthFrameColor = cv2.normalize(depthFrame, None, 255, 0, cv2.NORM_INF, cv2.CV_8UC1)
+        depthFrameColor = cv2.equalizeHist(depthFrameColor)
+        depthFrameColor = cv2.applyColorMap(depthFrameColor, cv2.COLORMAP_MAGMA)
+
+        detections = inDet.detections
+
+        # If the frame is available, draw bounding boxes on it and show the frame
+        height = frame.shape[0]
+        width  = frame.shape[1]
+      
+        
+        for detection in detections:
+            start_time = time.time()
+            roiData = detection.boundingBoxMapping
+            roi = roiData.roi
+            roi = roi.denormalize(depthFrameColor.shape[1], depthFrameColor.shape[0])
+            topLeft = roi.topLeft()
+            bottomRight = roi.bottomRight()
+            xmin = int(topLeft.x)
+            ymin = int(topLeft.y)
+            xmax = int(bottomRight.x)
+            ymax = int(bottomRight.y)
+            cv2.rectangle(depthFrameColor, (xmin, ymin), (xmax, ymax), color, cv2.FONT_HERSHEY_SCRIPT_SIMPLEX)
+
+            # Denormalize bounding box
+            x1 = int(detection.xmin * width)
+            x2 = int(detection.xmax * width)
+            y1 = int(detection.ymin * height)
+            y2 = int(detection.ymax * height)
+            zin = int(detection.spatialCoordinates.z)
+
+            if labelMap[detection.label] == "face":
+                bodyMovement((x1+x2)/2, (y1+y2)/2, zin)
+                print(zin)
+
+
+            try:
+                label = labelMap[detection.label]
+            except:
+                label = detection.label
+            cv2.putText(frame, str(label), (x1 + 10, y1 + 20), cv2.FONT_HERSHEY_TRIPLEX, 0.5, 255)
+            cv2.putText(frame, "{:.2f}".format(detection.confidence*100), (x1 + 10, y1 + 35), cv2.FONT_HERSHEY_TRIPLEX, 0.5, 255)
+            cv2.putText(frame, f"X: {int(detection.spatialCoordinates.x)} mm", (x1 + 10, y1 + 50), cv2.FONT_HERSHEY_TRIPLEX, 0.5, 255)
+            cv2.putText(frame, f"Y: {int(detection.spatialCoordinates.y)} mm", (x1 + 10, y1 + 65), cv2.FONT_HERSHEY_TRIPLEX, 0.5, 255)
+            cv2.putText(frame, f"Z: {int(detection.spatialCoordinates.z)} mm", (x1 + 10, y1 + 80), cv2.FONT_HERSHEY_TRIPLEX, 0.5, 255)
+
+            cv2.rectangle(frame, (x1, y1), (x2, y2), (255, 0, 0), cv2.FONT_HERSHEY_SIMPLEX)
+            elapsed_time = time.time() - start_time
+
+        cv2.putText(frame, "NN fps: {:.2f}".format(fps), (2, frame.shape[0] - 4), cv2.FONT_HERSHEY_TRIPLEX, 0.4, (255,255,255))
+        cv2.imshow("depth", depthFrameColor)
+        cv2.imshow("preview", frame)  
+
+                                
+
+        if cv2.waitKey(1) == ord('q'):
+            break
