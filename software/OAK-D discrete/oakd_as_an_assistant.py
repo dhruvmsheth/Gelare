560851d5c508abeea44976165a67a14dab569632
Dhruv Sheth <dhruvsheth.linkit@gmail.com>
2023-02-12 20:17:59 +0530

    Create oakd_as_an_assistant.py

---
+++ software/OAK-D discrete/oakd_as_an_assistant.py
@@ -0,0 +1,157 @@
+#!/usr/bin/env python
+from __future__ import print_function
+
+import device_patches       # Device specific patches for Jetson Nano (needs to be before importing cv2)
+
+import cv2
+import os
+import sys, getopt
+import signal
+import time
+from edge_impulse_linux.image import ImageImpulseRunner
+
+import argparse
+import cv2
+import depthai as dai
+
+
+# Start defining a pipeline
+pipeline = dai.Pipeline()
+
+# Define a source - color camera
+camRgb = pipeline.createColorCamera()
+camRgb.setPreviewSize(300, 300)
+camRgb.setBoardSocket(dai.CameraBoardSocket.RGB)
+camRgb.setResolution(dai.ColorCameraProperties.SensorResolution.THE_1080_P)
+camRgb.setInterleaved(False)
+camRgb.setColorOrder(dai.ColorCameraProperties.ColorOrder.RGB)
+
+# Create output
+xoutRgb = pipeline.createXLinkOut()
+xoutRgb.setStreamName("rgb")
+camRgb.preview.link(xoutRgb.input)
+
+runner = None
+# if you don't want to see a camera preview, set this to False
+show_camera = True
+if (sys.platform == 'linux' and not os.environ.get('DISPLAY')):
+    show_camera = False
+
+def now():
+    return round(time.time() * 1000)
+
+def get_webcams():
+    port_ids = []
+    for port in range(5):
+        print("Looking for a camera in port %s:" %port)
+        camera = cv2.VideoCapture(port)
+        if camera.isOpened():
+            ret = camera.read()[0]
+            if ret:
+                backendName =camera.getBackendName()
+                w = camera.get(3)
+                h = camera.get(4)
+                print("Camera %s (%s x %s) found in port %s " %(backendName,h,w, port))
+                port_ids.append(port)
+            camera.release()
+    return port_ids
+
+def sigint_handler(sig, frame):
+    print('Interrupted')
+    if (runner):
+        runner.stop()
+    sys.exit(0)
+
+signal.signal(signal.SIGINT, sigint_handler)
+
+def help():
+    print('python classify.py <path_to_model.eim> <Camera port ID, only required when more than 1 camera is present>')
+
+def main(argv):
+    try:
+        opts, args = getopt.getopt(argv, "h", ["--help"])
+    except getopt.GetoptError:
+        help()
+        sys.exit(2)
+
+    for opt, arg in opts:
+        if opt in ('-h', '--help'):
+            help()
+            sys.exit()
+
+    if len(args) == 0:
+        help()
+        sys.exit(2)
+
+    model = args[0]
+
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    modelfile = os.path.join(dir_path, model)
+
+    print('MODEL: ' + modelfile)
+
+    with ImageImpulseRunner(modelfile) as runner:
+        try:
+            model_info = runner.init()
+            print('Loaded runner for "' + model_info['project']['owner'] + ' / ' + model_info['project']['name'] + '"')
+            labels = model_info['model_parameters']['labels']
+            if len(args)>= 2:
+                videoCaptureDeviceId = int(args[1])
+            else:
+                port_ids = get_webcams()
+                if len(port_ids) == 0:
+                    raise Exception('Cannot find any webcams')
+                if len(args)<= 1 and len(port_ids)> 1:
+                    raise Exception("Multiple cameras found. Add the camera port ID as a second argument to use to this script")
+                videoCaptureDeviceId = int(port_ids[0])
+
+
+            # Pipeline defined, now the device is connected to
+            with dai.Device(pipeline) as device:
+                # Start pipeline
+                device.startPipeline()
+
+                # Output queue will be used to get the rgb frames from the output defined above
+                qRgb = device.getOutputQueue(name="rgb", maxSize=4, blocking=False)
+
+                while True:
+                    inRgb = qRgb.get()  # blocking call, will wait until a new data has arrived
+
+                    # Retrieve 'bgr' (opencv format) frame
+                    #cv2.imshow("bgr", inRgb.getCvFrame())
+                    frame = inRgb.getCvFrame()
+                    ret = frame
+
+                    next_frame = 0 # limit to ~10 fps here
+
+                    for res, img in runner.classifier(videoCaptureDeviceId):
+                        if (next_frame > now()):
+                            time.sleep((next_frame - now()) / 1000)
+
+                        # print('classification runner response', res)
+
+                        if "classification" in res["result"].keys():
+                            print('Result (%d ms.) ' % (res['timing']['dsp'] + res['timing']['classification']), end='')
+                            for label in labels:
+                                score = res['result']['classification'][label]
+                                print('%s: %.2f\t' % (label, score), end='')
+                            print('', flush=True)
+
+                        elif "bounding_boxes" in res["result"].keys():
+                            print('Found %d bounding boxes (%d ms.)' % (len(res["result"]["bounding_boxes"]), res['timing']['dsp'] + res['timing']['classification']))
+                            for bb in res["result"]["bounding_boxes"]:
+                                print('\t%s (%.2f): x=%d y=%d w=%d h=%d' % (bb['label'], bb['value'], bb['x'], bb['y'], bb['width'], bb['height']))
+                                img = cv2.rectangle(img, (bb['x'], bb['y']), (bb['x'] + bb['width'], bb['y'] + bb['height']), (255, 0, 0), 1)
+
+                        if (show_camera):
+                            cv2.imshow('edgeimpulse', cv2.cvtColor(img, cv2.COLOR_RGB2BGR))
+                            if cv2.waitKey(1) == ord('q'):
+                                break
+
+                        next_frame = now() + 100
+        finally:
+            if (runner):
+                runner.stop()
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
